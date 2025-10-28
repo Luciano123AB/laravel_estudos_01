cb00f49f3b6b70ce31b9819478a0fe0534a733f7
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-28 18:45:55 -0300

    008(ControllerCriado_Laravel)

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function cleanUpperCaseString($string): string {
+        // Remover os espaços em branco no início e fim de uma string.
+        // Converter a string para uppercase. (maiúsculas)
+        return strtoupper(trim($string));
+    }
 }
