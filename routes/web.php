30b5df66bf5ee5b44f64869621af9729a546222f
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-29 20:11:17 -0300

    010(AtribuirMiddlewares_Rotas)

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\EndMiddleware;
+use App\Http\Middleware\StartMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", [MainController::class, "index"])->name("index");
-Route::get("/about", [MainController::class, "about"])->name("about");
+// Route::get("/", [MainController::class, "index"])->name("index");
+// Route::get("/about", [MainController::class, "about"])->name("about");
+// Route::get("/contact", [MainController::class, "contact"])->name("contact");
+
+Route::get("/", [MainController::class, "index"])->name("index")->middleware([StartMiddleware::class]);
+Route::get("/about", [MainController::class, "about"])->name("about")->middleware([StartMiddleware::class, EndMiddleware::class]);
 Route::get("/contact", [MainController::class, "contact"])->name("contact");
