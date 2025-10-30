c56d1bc685dfa8c2c6746c3e33f1d1e4493a05cd
Luciano Eduardo <luciano123ab@gmail.com>
2025-10-29 21:18:14 -0300

    011(Middlewares_Globais)

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,14 @@ Route::get("/about", [MainController::class, "about"])->name("about")->middlewar
 Route::get("/contact", [MainController::class, "contact"])->name("contact");
 */
 
-Route::middleware([StartMiddleware::class, EndMiddleware::class])->group(function() {
+// Route::middleware([StartMiddleware::class, EndMiddleware::class])->group(function() {
+//     Route::get("/", [MainController::class, "index"])->name("index");
+//     Route::get("/about", [MainController::class, "about"])->name("about")->withoutMiddleware([EndMiddleware::class]);
+//     Route::get("/contact", [MainController::class, "contact"])->name("contact");
+// });
+
+Route::middleware(["correr_depois"])->group(function() {
     Route::get("/", [MainController::class, "index"])->name("index");
-    Route::get("/about", [MainController::class, "about"])->name("about")->withoutMiddleware([EndMiddleware::class]);
+    Route::get("/about", [MainController::class, "about"])->name("about");
     Route::get("/contact", [MainController::class, "contact"])->name("contact");
 });
